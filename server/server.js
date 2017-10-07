0903bffcf26714bcc5bd45fcdb232bf9f1305300
zhaoyangwuuuu <zhaoyang1018@hotmail.com>
2017-10-07 07:10:15 -0400

    add css for chat page'

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ io.on('connection', (socket) => {
     console.log('createMessage', message);
 
     io.emit('newMessage', generateMessage(message.from, message.text));
-    callback('this is from the server.');
+    callback();
   });
 
   socket.on('createLocationMessage', (coords) => {
